eb5cbfd15ee48dcb4b76cf2f4442cd5d316c3728
Lidia Parrilla <lidiaparrilla22@gmail.com>
2021-04-22 17:38:02 +0200

    Reading text on button click

--- test/gui.py
+++ test/gui.py
@@ -4,10 +4,14 @@ class LedTest:
 	leds = []
 	rows = 8
 	columns = 18
+	window = object()
+	txt = object()
 
 	def clicked(self):
 		#TODO: Start test
-		self.switch_led(2,4,True)
+		#self.switch_led(2,4,True)
+		input_text = self.txt.get()
+		print(input_text)
 
 	def switch_led(self, row, column, state):
 		led=self.leds[row][column]
@@ -18,26 +22,26 @@ class LedTest:
 
 	def main(self):
 		# Define the leds height and width
-		window = Tk()
-		window.title("Led test")
-		window.geometry('400x200')
+		self.window = Tk()
+		self.window.title("Led test")
+		self.window.geometry('400x200')
 
-		txt = Entry(window, width=30)
-		txt.grid(column=0, row=0, columnspan=14)
+		self.txt = Entry(self.window, width=30)
+		self.txt.grid(column=0, row=0, columnspan=14)
 
-		btn = Button(window, text="Start", command=self.clicked)
+		btn = Button(self.window, text="Start", command=self.clicked)
 		btn.grid(column=14, row=0, columnspan=4)
 
 		# Create leds elements
 		for c in range(self.columns):
 			row = []
 			for r in range(self.rows):
-				new_label = Label(window, text = " ", bg = "white")
+				new_label = Label(self.window, text = " ", bg = "white")
 				new_label.grid(column=c, row=r+1, padx=1, pady=1)
 				row.append(new_label)
 			self.leds.append(row)
 
-		window.mainloop()
+		self.window.mainloop()
 
 test = LedTest()
 test.main()
